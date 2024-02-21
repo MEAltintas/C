94328a9ebaf4fa8fc6b818c0d8d8e74ec5b9c6fb
Münüre Ezgi Altıntaş <101413474+MEAltintas@users.noreply.github.com>
2024-02-21 17:51:40 +0300

    Char Kullanımı
    Char kullanımında kopyalama işlemi örneğidir.

---
+++ char4.c
@@ -0,0 +1,30 @@
+
+#include <stdio.h>
+
+int
+main ()
+{
+  char metin1[20] = "Merhaba";
+  char metin2[] = "Dünya!";
+  char metin3[20];
+
+  strcat (metin1, metin2);		// metin2'yi metin1'in ucuna ekle(sonuç metin1'de depolanır)
+
+  printf ("%s", metin1);
+  printf ("\n...\n");
+
+  strcpy (metin3, metin1);		// metin1'i metin3'e kopyalar
+  printf ("%s", metin3);
+  printf ("\n...\n");
+
+
+  printf ("%d\n", strcmp (metin1, metin3));	// strcmp karşılaştırma için kullanılır.
+  printf ("\n...\n");
+  printf ("%d\n", strcmp (metin1, metin2));	
+  printf ("\n...\n");
+
+  printf ("%d\n", strcmp (metin2, metin1));	
+  printf ("\n...\n");
+
+  return 0;
+}
