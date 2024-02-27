bc64f2cc86f31f42a71f7f2f6755f320f773c41c
Münüre Ezgi Altıntaş <101413474+MEAltintas@users.noreply.github.com>
2024-02-28 00:26:30 +0300

    True-False
    Çıktı olarak 1,0 değerler döndürür.

---
+++ booleanOrnek1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+
+int main() {
+   bool programcilikGuzelMi=true;
+   bool tembellikİyiMi=false;
+   printf("%d\n",programcilikGuzelMi);
+   printf("%d\n",tembellikİyiMi);
+   printf("\n...\n");
+   
+    return 0;
+}
