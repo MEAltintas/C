370263303d4d356b33a586b87af63e53c1e1ad68
Münüre Ezgi Altıntaş <101413474+MEAltintas@users.noreply.github.com>
2024-02-28 00:28:26 +0300

    True-False
    Çıktı olarak iki sonuçta 1 değeri döndürür.

---
+++ booleanOrnek2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+
+int main() {
+   
+   printf("%d\n",10>9);
+   int x=10;
+   int y=9;
+   printf("%d\n",x>y);
+   printf("\n...\n");
+   
+    return 0;
+}
