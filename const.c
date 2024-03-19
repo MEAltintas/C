91926d0bff0201c9184fd4c5970488793b6a8cf5
Münüre Ezgi Altıntaş <101413474+MEAltintas@users.noreply.github.com>
2024-03-20 01:29:14 +0300

    Const
    const kullanımıdır. Yani sabitler, değişmez anlamına gelir.

---
+++ const.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    const int saattekiDakikaSayisi=60;
+    const float PI=3.14;
+    
+    printf("%d\n",saattekiDakikaSayisi);
+    printf("%f\n",PI);
+ 
+    return 0;
+}
