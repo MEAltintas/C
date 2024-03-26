d867061f2db21fa66964fcd78657ce1671ef5e24
Münüre Ezgi Altıntaş <101413474+MEAltintas@users.noreply.github.com>
2024-03-26 15:45:28 +0300

    Depolama Konum Belirteci-Auto
    Auto depolama sınıfı, tüm yerel değişkenler için varsayılan depolama sınıfıdır.

---
+++ auto.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void degerArtir(int k){
+    auto int i=0;
+    printf("%d ",i);
+    i+=k;
+}
+
+int main() {
+   degerArtir(3);
+   degerArtir(3);
+   degerArtir(3);
+   degerArtir(3);
+    return 0;
+}
