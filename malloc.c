eb7840cf5373c4d3bd0e9e69bf4f2339d412674c
Münüre Ezgi Altıntaş <101413474+MEAltintas@users.noreply.github.com>
2024-03-26 15:15:14 +0300

    Malloc Fonksiyonu
    Malloc: Boyutu byte olarak ifade edilen size parametre değeri kadar bir bellek bloğunu tahsis eder ve bellek bloğunun başlangıç adresini gösteren bir işaretçi geri döndürür.

---
+++ malloc.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int n,i,*isaretci,toplam=0;
+    printf("Toplanacak eleman sayısını giriniz: ");
+    scanf("%d",&n);
+    
+    isaretci=(int*)malloc(n*sizeof(int));
+    
+    // Bellek ayrımının başarısız olma durumunda
+    if(isaretci==NULL){
+        printf("Hata! Bellek ayırma işlemi başarısız oldu.");
+        exit(0);
+    }
+    printf("Toplanacak elemanları giriniz:\n");
+    for(i=0; i<n; ++i){
+        scanf("%d",isaretci+i);
+        toplam+=*(isaretci+i);
+    }
+    printf("Toplam = %d" ,toplam);
+    
+    //ayrılmış olan belleği serbest bırak
+    free(isaretci);
+    
+    return 0;
+}
