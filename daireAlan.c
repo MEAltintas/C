7c7c63420e880944c62c034c8c2e9bb1d4ba5d15
Münüre Ezgi Altıntaş <101413474+MEAltintas@users.noreply.github.com>
2024-04-15 23:25:03 +0300

    Dairenin Alanı ve Çevresi
    yarı çapı dışardan girilen bir dairenin çevresini ve alanını bulan program.

---
+++ daireAlan.c
@@ -0,0 +1,25 @@
+
+#include <stdio.h>
+#include <conio.h>
+
+#define pi 3.14
+
+int main()
+{
+   //float p = 3.14;
+  
+    float r , cevre, alan ;
+    
+    printf("Yaricapi girin :");
+    
+    scanf("%f", &r);
+    
+    
+    cevre=2*pi*r;
+    
+    alan=pi*r*r;
+    
+    printf ("Cevre olcusu : %f Alan olcusu : %f \n"  , cevre, alan );
+           
+    return 0;
+}
