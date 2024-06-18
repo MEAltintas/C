15d94c8b798169938b268efbfb84807357fff1b7
Münüre Ezgi Altıntaş <101413474+MEAltintas@users.noreply.github.com>
2024-06-18 16:56:15 +0300

    Geçti-Kaldı Programı
    Girilen üç nottan en az iki tanesi 60 veya üzeri ise ekrana “Geçti” yazan, diğer durumlarda ise “Kaldı” yazan kod

---
+++ gectikaldi.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+  int nota, notb, notc;
+  printf("Birinci notu girin:");
+  scanf("%d",&nota);
+  printf("ikinci notu girin:");
+  scanf("%d",&notb);
+  printf("ucuncu notu girin:");
+  scanf("%d",&notc);
+
+  if ((nota>=60 && notb>=60) || (nota>=60 && notc>=60) || (notb>=60 && notc>=60) )
+    printf("Gecti");
+  else
+    printf("Kaldı");
+  return 0;
+}
